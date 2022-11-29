e866c5e363733afc345c2220538902286ebac6a7
JoanaCastaneda <dcg.joana@hotmail.com>
2022-11-28 21:54:27 -0500

    Hooking up character anima5ons

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -127,6 +127,9 @@ public class SoyBoyController : MonoBehaviour
         // 1
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Jump");
+
+
+        animator.SetFloat("Speed", Mathf.Abs(input.x));
         // 2
         if (input.x > 0f)
         {
@@ -142,10 +145,12 @@ public class SoyBoyController : MonoBehaviour
         if (input.y >= 1f)
         {
             jumpDuration += Time.deltaTime;
+            animator.SetBool("IsJumping", true);
         }
         else
         {
             isJumping = false;
+            animator.SetBool("IsJumping", false);
             jumpDuration = 0f;
         }
         ///
@@ -159,6 +164,9 @@ public class SoyBoyController : MonoBehaviour
             {
                 isJumping = true;
             }
+
+            animator.SetBool("IsOnWall", false);
+
         }
 
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -207,11 +215,21 @@ public class SoyBoyController : MonoBehaviour
         // 4
         rb.velocity = new Vector2(xVelocity, yVelocity);
 
-        if (IsWallToLeftOrRight() && !PlayerIsOnGround()
- && input.y == 1)
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
         {
-            rb.velocity = new Vector2(-GetWallDirection()
-            * speed * 0.75f, rb.velocity.y);
+            rb.velocity = new Vector2(-GetWallDirection() * speed
+            * 0.75f, rb.velocity.y);
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        else if (!IsWallToLeftOrRight())
+        {
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround())
+        {
+            animator.SetBool("IsOnWall", true);
         }
 
 
